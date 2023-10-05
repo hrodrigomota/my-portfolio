5ed37ad3fafb353675a136ab6d9f35d459f40f07
Hilton Rodrigo Mota <hrodrigomota@gmail.com>
2023-10-05 17:23:39 -0300

    Data Update

--- src/components/Projects/index.tsx
+++ src/components/Projects/index.tsx
@@ -2,6 +2,7 @@ import { Page } from "../template/Page";
 import { Card } from "./Card";
 import { TechCard } from "./Card/TechCard";
 
+import AttendanceList from "../../assets/attendanceList.png"
 import ToDoDaily from "../../assets/toDoDaily.png";
 import StaartApp from "../../assets/staart-front-end-app.png";
 import ConsumingAPI from "../../assets/consumingAPI.png";
@@ -18,6 +19,43 @@ export function Projects() {
           PROJETOS
         </h1>
         <div className="flex flex-wrap justify-center gap-x-10 gap-y-6">
+          <Card
+            src={AttendanceList}
+            alt="Attendance List Screen"
+            href="https://attendance-list-pro.vercel.app/"
+            hrefRepo="https://github.com/hrodrigomota/attendance-list"
+            name="Attendance List"
+            description="O Attendance List consiste em um modelo de lista de presença que pode ser utilizado para fins profissionais, no qual os usuários preenchem um formulário com o seu nome, e-mail e profissão para confirmar a sua presença, o que pode gerar um possível lead."
+          >
+            <TechCard 
+              href="https://react.dev/" 
+              name="React" 
+            />
+            <TechCard
+              href="https://developer.mozilla.org/pt-BR/docs/Web/CSS"
+              name="CSS modules"
+            />
+            <TechCard
+              href="https://www.radix-ui.com/"
+              name="Radix UI"
+            />
+            <TechCard
+              href="https://phosphoricons.com/"
+              name="Phosphor Icons"
+            />
+            <TechCard
+              href="https://www.typescriptlang.org/"
+              name="TypeScript"
+            />
+            <TechCard
+              href="https://github.com/uuidjs/uuid#readme"
+              name="uuid"
+            />
+            <TechCard 
+              href="https://vitejs.dev/" 
+              name="Vite" 
+            />
+          </Card>
           <Card
             src={ToDoDaily}
             alt="To Do Daily Screen"
@@ -63,7 +101,7 @@ export function Projects() {
             href="https://hrodrigomota.github.io/consuming-API-v2/"
             hrefRepo="https://github.com/hrodrigomota/consuming-API-v2"
             name="Consuming API"
-            description="O Consuming API consiste num projeto que visa a prática do consumo de Application Programming Interface - API, juntamente com o paradigma da Programação Orientada ao Objeto - POO, onde, utilizou-se a Chuck Norris IO e a PokéAPI."
+            description="O Consuming API consiste num projeto que visa a prática do consumo de Application Programming Interface - API, juntamente com o paradigma da Programação Orientada a Objetos - POO. Neste projeto, foram utilizadas a Chucknorris.io e a PokéAPI."
           >
             <TechCard
               href="https://developer.mozilla.org/pt-BR/docs/Web/HTML"
@@ -132,7 +170,7 @@ export function Projects() {
             href="https://hrodrigomota.github.io/flash/"
             hrefRepo="https://github.com/hrodrigomota/flash"
             name="Flash"
-            description="O Flash foi desenvolvido durante o evento Quick Staart da plataforma Staart. Em formato de quiz, visa testar o vocabulário técnico do desenvolvedor, onde, perguntas e respostas são recebidas através de uma API disponibilizada para a criação do projeto."
+            description="O Flash foi desenvolvido durante o evento Quick Staart da plataforma Staart. Em formato de quiz, visa testar o vocabulário técnico do desenvolvedor, no qual perguntas e respostas são recebidas através de uma API disponibilizada para a criação do projeto."
           >
             <TechCard
               href="https://developer.mozilla.org/pt-BR/docs/Web/HTML"
